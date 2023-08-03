99f3219d782bdddbe20c7f10f85d4947c2e7005f
Mason Cole <cole.mason2@gmail.com>
2023-08-02 20:11:22 -0500

    updates

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import crypto from '../assets/cryptostation.jpeg'
-import restaurant from '../assets/rest-res.jpeg'
-import grubdash from '../assets/grubdash.jpeg'
-import flashcards from '../assets/flashcards.jpeg'
-
+import crypto from "../assets/cryptostation.jpeg";
+import restaurant from "../assets/rest-res.jpeg";
+import grubdash from "../assets/grubdash.jpeg";
+import flashcards from "../assets/flashcards.jpeg";
+import netflix from '../assets/netflix.jpeg'
 
 const Work = () => {
   return (
@@ -58,6 +58,30 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/MasonCole14/starter-restaurant-reservation">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/*Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${netflix})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Netflix App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://netflix-app-ebon-beta.vercel.app">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/MasonCole14/netflix-app">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
